b287ac7ee0069f1bafcf6bb22d9fce110aa17146
Jess <jessjo.324@gmail.com>
2025-04-01 21:38:03 -0500

    Start Carousel design

---
+++ src/app/features/booking/components/Carrousel/Carrousel.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'carrousel',
+  imports: [],
+  templateUrl: './Carrousel.component.html',
+})
+export class CarrouselComponent { }
--- src/app/features/booking/pages/billboard-page/billboard-page.component.ts
+++ src/app/features/booking/pages/billboard-page/billboard-page.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { DropdownButtonComponent } from "../../../../shared/dropdown-button/dropdown-button.component";
+import { CarrouselComponent } from "../../components/Carrousel/Carrousel.component";
 
 @Component({
   selector: 'app-billboard-page',
-  imports: [DropdownButtonComponent],
+  imports: [DropdownButtonComponent, CarrouselComponent],
   templateUrl: './billboard-page.component.html',
 })
 export class BillboardPageComponent { }
